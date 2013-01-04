028371bdb06511c2eef24ed0d993e93bb094336a
Danielle Sucher <dsucher@gmail.com>
2013-01-04 11:19:09 -0500

    Set correct default port for Flask on Heroku

--- app.py
+++ app.py
@@ -179,6 +179,5 @@ class Book(db.Model):
 
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 33507))
     app.run(host='0.0.0.0', port=port)
